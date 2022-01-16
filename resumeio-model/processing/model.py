07dda114ee25d85eb485246945621d99fbc1567f
erhan <erhan.mamut@esolutions.ro>
2022-01-16 17:14:06 +0200

    fix

--- resumeio-model/processing/model.py
+++ resumeio-model/processing/model.py
@@ -1,9 +1,6 @@
 import gensim.downloader as api
 import numpy as np
 
-import cv_parser
-import job_parser
-
 model = api.load("glove-wiki-gigaword-300")
 
 
@@ -29,22 +26,19 @@ def encode_words(word_list):
     return codifications
 
 
-def match():
-    print("START")
-    candidates = cv_parser.extract_cv_data()
-    job_description = job_parser.extract_job_data()
-    job_encoded = list(np.append(job_description.description, job_description.requirements))
-    job_encoded = encode_words(job_encoded)
-    rank = {}
-    for candidate in candidates:
-        candidate_encoded = list(np.append(candidate.skills, candidate.experience))
-        candidate_encoded = encode_words(candidate_encoded)
-        similarities = find_similarities(candidate_encoded, job_encoded)
-        if len(candidate_encoded) != 0:
-            rank[candidate.name] = len(similarities) / len(candidate_encoded)
-        print(similarities)
-    rank = [(k, v) for k, v in sorted(rank.items(), key=lambda item: item[1],  reverse=True)]
-    print(rank)
-
-
-match()
+# def match():
+#     print("START")
+#     candidates = cv_parser.extract_cv_data()
+#     job_description = job_parser.extract_job_data()
+#     job_encoded = list(np.append(job_description.description, job_description.requirements))
+#     job_encoded = encode_words(job_encoded)
+#     rank = {}
+#     for candidate in candidates:
+#         candidate_encoded = list(np.append(candidate.skills, candidate.experience))
+#         candidate_encoded = encode_words(candidate_encoded)
+#         similarities = find_similarities(candidate_encoded, job_encoded)
+#         if len(candidate_encoded) != 0:
+#             rank[candidate.name] = len(similarities) / len(candidate_encoded)
+#         print(similarities)
+#     rank = [(k, v) for k, v in sorted(rank.items(), key=lambda item: item[1],  reverse=True)]
+#     print(rank)
--- resumeio-model/server.py
+++ resumeio-model/server.py
@@ -1,26 +1,12 @@
-import pymongo
 from flask import Flask
-
-import config
+from processing import model
 
 app = Flask(__name__)
-if app.config["ENV"] == "production":
-    app.config.from_object(config.ProductionConfig)
-else:
-    app.config.from_object(config.DevelopmentConfig)
-
-client = pymongo.MongoClient(app.config["DATABASE_URI"])
-db = client.get_database('resumeiomodel')
-
-if "scores" not in db.list_collection_names():
-    print("== create scores ==")
-    db.create_collection("scores")
 
 
 @app.route("/")
 def hello_world():
-    db.get_collection("scores").insert_one(
-        {"id": 1, "name": "name"})
+    model.encode_words(['foot', 'job', 'IT', 'ball'])
     return "<p>Hello, World!</p>"
 
 
